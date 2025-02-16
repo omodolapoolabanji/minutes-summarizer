eb1b9b711125b903db71bbd0847291270879dfb4
omodolapoolabanji <bolabanj@iu.edu>
2025-02-15 23:21:49 -0500

    init commit

---
+++ backend/repositories/SummaryRepo.ts
@@ -0,0 +1,24 @@
+import Summary from "../models/Summary";
+
+export default class SummaryRepo{
+    summaries : Map<Number, Summary>
+    constructor(){
+        this.summaries = new Map(); 
+    }
+
+    addSummary(summary: Summary){
+        this.summaries.set(summary.id, summary);
+    }
+
+    getSummaryById(id: Number){
+        try{
+            return this.summaries.get(id); 
+        }catch(error: any){
+            console.error("Error retrieving summary by id", error);
+        }
+    }
+
+    getAllSummaries(): Array<Summary>{
+        return Array.from(this.summaries.values());
+    }
+}
